e7d322a4247960e6fd9c367174763be596429d8e
harounatraore894-art <harounatraore894@gmail.com>
2026-06-19 20:09:04 +0100

    second commit

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def post_list1(request):
+    return render(request, 'blog/post_list1.html', {})
 # Create your views here.
